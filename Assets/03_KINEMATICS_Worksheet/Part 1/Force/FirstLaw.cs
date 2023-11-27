54b5b6cd6ca9e7a7248beb135b14ae289968d124
sevencode-05 <116236897+sevencode-05@users.noreply.github.com>
2023-11-27 14:18:50 +0800

    Update

--- Assets/03_KINEMATICS_Worksheet/Part 1/Force/FirstLaw.cs
+++ Assets/03_KINEMATICS_Worksheet/Part 1/Force/FirstLaw.cs
@@ -1,21 +1,21 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class FirstLaw : MonoBehaviour
-// {
-//     public Vector3 force;
-//     Rigidbody rb;
+public class FirstLaw : MonoBehaviour
+ {
+     public Vector3 force;
+     Rigidbody rb;
 
-//     void Start()
-//     {
-//         rb = // your code here;
-//         rb.// your code here;
-//     }
+    void Start()
+     {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(force, ForceMode.Force);
+     }
 
-//     void FixedUpdate()
-//     {
-//         Debug.Log(transform.position);
-//     }
-// }
+     void FixedUpdate()
+     {
+         Debug.Log(transform.position);
+     }
+}
 
--- Assets/03_KINEMATICS_Worksheet/Part 2 POOL/Scripts/Table2D.cs
+++ Assets/03_KINEMATICS_Worksheet/Part 2 POOL/Scripts/Table2D.cs
@@ -2,35 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Table2D : MonoBehaviour
-{
-    public List<Ball2D> balls;
+//public class Table2D : MonoBehaviour
+//{
+    //public List<Ball2D> balls;
 
-    private void Start()
-    {
+    //private void Start()
+    //{
 
-    }
+    //}
 
-    bool CheckBallCollision(Ball2D toCheck)
-    {
+    //bool CheckBallCollision(Ball2D toCheck)
+    //{
         //for (int i = 0; i < balls.Count; i++)
-        {
-            Ball2D ball = balls[1];
+        //{
+            //Ball2D ball = balls[1];
 
-            if (ball.IsCollidingWith(toCheck) && toCheck != ball)
-            {
-                return true;
-            }
-        }
+            //if (ball.IsCollidingWith(toCheck) && toCheck != ball)
+            //{
+                //return true;
+            //}
+        //}
 
-        return false;
-    }
+        //return false;
+    //}
 
-    private void FixedUpdate()
-    {
-        if (CheckBallCollision(balls[0]))
-        {
-            Debug.Log("COLLISION!");
-        }
-    }
-}
+    //private void FixedUpdate()
+    //{
+        //if (CheckBallCollision(balls[0]))
+        //{
+            //Debug.Log("COLLISION!");
+        //}
+    //}
+//}
